f0d4f20a32fa6e38f89819fa3f5582d10dd0805b
Rehemu Y Wulugebige <rehemu.wulugebige@gmail.com>
2026-08-19 15:25:00 -0400

    Implement create customer and get customer

--- routes/customers.js
+++ routes/customers.js
@@ -9,12 +9,42 @@ const customerSchema = new mongoose.Schema({
     phone: String, 
 })
 
-const Customer = mongoose.model('Customer', cutomerSchema)
+const Customer = mongoose.model('Customer', customerSchema)
 
-// router.get('/', async (req, res) => {
-//     const customers = Customer.find()
-//     res.send(customers)
-// })
+router.post('/', async (req, res) => {
+    const { error } = validateCustomer(req.body)
+
+    if (error) {
+        res.status(400).send(error.details[0].message)
+    }
+
+    let customer = new Customer({
+        isGold: req.body.isGold, 
+        name: req.body.name, 
+        phone: req.body.phone
+    })
+
+    customer = await customer.save()
+
+    res.send(customer)
+})
+
+// Get all the customer in the database
+router.get('/', async (req, res) => {
+    const customers = await Customer.find()
+    res.send(customers)
+})
+
+// Get the cutomer with the given ID
+router.get('/:id', async (req, res) => {
+    const customer = await Customer.findById(req.params.id)
+
+    if (!customer) {
+        return res.status(404).send('Customer with the given ID was NOT found')
+    }
+
+    res.send(customer)
+})
 
 function validateCustomer(customer) {
     const schema = Joi.object({
