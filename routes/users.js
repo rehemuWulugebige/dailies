3628c2ef66db1f3187f7549d2be48d60e9a6d06a
Rehemu Y Wulugebige <rehemu.wulugebige@gmail.com>
2026-09-01 13:37:45 -0400

    Implement get user

--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,29 @@ router.post("/", async (req, res) => {
         return res.status(400).send(error.details[0].message)
     }
 
-    let user = new User({
+    let user = await User.findOne({ email: req.body.email })
+
+    if (user) {
+        return res.status(400).send("User is already registered")
+    }
+
+    user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
     })
 
-    user = await user.save()
+    await user.save()
+
+    res.send(user)
+})
+
+router.get("/", async (req, res) => {
+    const user = await User.findById(req.body._id)
+
+    if (!user) {
+        return res.status(404).send("User with the given ID was not found")
+    }
 
     res.send(user)
 })
