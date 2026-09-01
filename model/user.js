90659356b9911bdc83d47e69265d04c5d2f4b21d
Rehemu Y Wulugebige <rehemu.wulugebige@gmail.com>
2026-09-01 12:16:31 -0400

    Implement user model

---
+++ model/user.js
@@ -0,0 +1,34 @@
+const Joi = require("joi")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+})
+
+const User = new mongoose.model("User", userSchema)
+
+function validateUser(user) {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        password: Joi.string().required(),
+    })
+
+    const result = schema.validate(user)
+
+    return result
+}
+
+module.exports.validate = validateUser
+module.exports.User = User
